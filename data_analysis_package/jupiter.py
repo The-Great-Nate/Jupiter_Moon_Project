6d94dd7695de38d4a2b36d4d733762e33b7456a4
Nathan Truong <tnathan1388@gmail.com>
2024-01-24 07:24:15 +0000

    • Amended a few return type hints and added incline comments

--- data_analysis_package/jupiter.py
+++ data_analysis_package/jupiter.py
@@ -6,12 +6,14 @@ from IPython.display import display, HTML
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error
+from typing import Union, Tuple, Any
 
 
 class Moons:
     """
     This class holds a database regarding the moons which orbit a planet.
     """
+
     def __init__(self, database_path: str, table: str) -> None:
         """
         Creates a private attribute of the database created by SQL query. Uses the sqlite database service.
@@ -42,7 +44,6 @@ class Moons:
         :return: None
         """
         self.__database = df
-
 
     def display_data_info(self) -> None:
         """
@@ -92,9 +93,9 @@ class Moons:
         Returns correlation summary between variable pairs as dataframe.
         :return: Correlation summary between variable pairs
         """
-        return self.__database.corr(numeric_only = True)
+        return self.__database.corr(numeric_only=True)
 
-    def heatmap(self, square = True, vmin = -1, vmax = 1, cmap = "RdBu") -> None:
+    def heatmap(self, square=True, vmin=-1, vmax=1, cmap="RdBu") -> None:
         """
         Outputs heatmap based on correlation summary data frame
         :param square: Default:True.  Axes aspect to “equal” so each cell will be square-shaped.
@@ -103,11 +104,11 @@ class Moons:
         :param cmap: Default:"R dBu". Colourmap that data would map to.
         :return: None
         """
-        sns.heatmap(data = self.corr(), square = square, vmin = vmin, vmax = vmax, cmap = cmap)
+        sns.heatmap(data=self.corr(), square=square, vmin=vmin, vmax=vmax, cmap=cmap)
         plt.title("Correlation Heatmap")
         plt.show()
 
-    def return_moon_data(self, name: str) -> None | pd.DataFrame:
+    def return_moon_data(self, name: str) -> Union[None, pd.DataFrame]:
         """
         Returns record of moon name is assigned to
         :param name: moon name to return record for.
@@ -119,7 +120,7 @@ class Moons:
         else:
             return self.__database[self.__database["moon"] == name]
 
-    def return_corr(self, col_1: str, col_2: str) -> None | int:
+    def return_corr(self, col_1: str, col_2: str) -> Union[None, int]:
         """
         Returns pearsons r value between two variables
         :param col_1: column 1
@@ -132,7 +133,7 @@ class Moons:
         else:
             return self.__database[col_1].corr(self.__database[col_2])
 
-    def plot_relationship(self, col_1: str, col_2: str, hue = None) -> None:
+    def plot_relationship(self, col_1: str, col_2: str, hue=None) -> None:
         """
         Returns None if neither parameter is a valid column name.
         Outputs either:
@@ -156,24 +157,26 @@ class Moons:
         is assumed to be categorical which is bad. 
         '''
         filter_nan = self.__database.dropna(subset=[col_1, col_2])
-        if pd.to_numeric(filter_nan[col_1], errors = "coerce").notna().all() == False and pd.to_numeric(filter_nan[col_2], errors = "coerce").notna().all() == False:
+        if pd.to_numeric(filter_nan[col_1], errors="coerce").notna().all() == False and pd.to_numeric(filter_nan[col_2],
+                                                                                                      errors="coerce").notna().all() == False:
             print("Comparing 2 categorical variables")
             sns.countplot(data=self.__database, x=col_1, hue=col_2)
-        elif pd.to_numeric(filter_nan[col_1], errors = "coerce").notna().all() == False or pd.to_numeric(filter_nan[col_2], errors = "coerce").notna().all() == False:
-            sns.catplot(data=self.__database, x=col_1, y=col_2, kind="box", aspect=1.5, hue = hue)
+        elif pd.to_numeric(filter_nan[col_1], errors="coerce").notna().all() == False or pd.to_numeric(
+                filter_nan[col_2], errors="coerce").notna().all() == False:
+            sns.catplot(data=self.__database, x=col_1, y=col_2, kind="box", aspect=1.5, hue=hue)
         else:
-            plot = sns.relplot(data=self.__database, x=col_1, y=col_2, hue = hue)
+            plot = sns.relplot(data=self.__database, x=col_1, y=col_2, hue=hue)
             plot.fig.suptitle(f"{col_1} x {col_2}", fontsize=16)
             plot.fig.subplots_adjust(top=0.9)
 
-    def pairwise_plots(self, hue = None) -> None:
+    def pairwise_plots(self, hue=None) -> None:
         """
         Outputs pairwise relationship plots in the dataset
         :param hue: Grouping variable that will produce elements in each plot with different colors.
          Ideally use a categorical variable.
         :return: None
         """
-        sns.pairplot(self.__database, hue = hue)
+        sns.pairplot(self.__database, hue=hue)
 
     def filter_for_characteristic(self, column: str, characteristic: str) -> pd.DataFrame:
         """
@@ -201,16 +204,17 @@ class Moons:
     Static methods are used to boost program performance. The @staticmethod decorator can be used on certain methods
     as some methods can be used independently no matter the instance of the class.
     '''
+
     @staticmethod
-    def return_central_mass(gradient: int) -> int:
+    def return_central_mass(gradient: int) -> float:
         """
         Uses Kepler's 3rd Law of Planetary Motion from a gradient to return central body mass.
         :param gradient: Gradient obtained from T^2 against a^3 plot.
         :return: Central Body Mass
         """
-        return (4*np.pi**2)/(6.67E-11 * gradient)
+        return (4 * np.pi ** 2) / (6.67E-11 * gradient)
 
-    def get_model(self, col_1: str, col_2: str, fit_intercept = True) -> tuple:
+    def get_model(self, col_1: str, col_2: str, fit_intercept=True) -> tuple:
         """
         Creates instance of LinearRegression that accepts training data from both columns and then fits. The training data
         created is through sklearn.model_selection's test_train_split() method. Due to
@@ -225,15 +229,26 @@ class Moons:
         :param fit_intercept: Default: True, assumes there is or is not an intercept to take into account
         :return: Tuple stated above
         """
-        model = linear_model.LinearRegression(fit_intercept = fit_intercept)
+        model = linear_model.LinearRegression(fit_intercept=fit_intercept)
+        '''
+        There is only 1 feature in the X data so the data takes the shape (samples, features = 1)
+        The target values only need 1 dimension.
+        '''
         x = self.__database[[col_1]]
         y = self.__database[col_2]
+
+        '''
+        To prevent overfitting and enable the model to create new data in any scanario rather than only relying
+        on the entire database only, the data is split into a training set and test set, where the test set is
+        30% and by default, the training size is set to "compliment the test size"
+        '''
         x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+        # Fitting the model to the training data created from train_test_split()
         model.fit(x_train, y_train)
         return model, x_test, y_test, col_1, col_2
 
     @staticmethod
-    def get_model_parameters(model: tuple) -> str | tuple:
+    def get_model_parameters(model: tuple) -> Union[str, tuple]:
         """
         Returns tuple of either warning message (String) or (int)
         :param model: Tuple generated from self.get_model(). In order,
@@ -250,7 +265,7 @@ class Moons:
         return model_obj.coef_[0], model_obj.intercept_
 
     @staticmethod
-    def predict(model:tuple) -> str | pd.array:
+    def predict(model: tuple) -> Union[str, tuple]:
         """
         Returns prediction from inputted model tuple generated from get_model()
         :param model: Tuple generated from self.get_model(). In order,
@@ -267,7 +282,7 @@ class Moons:
         return pred
 
     @staticmethod
-    def plot_prediction(model:tuple, pred: pd.array) -> str | None:
+    def plot_prediction(model: tuple, pred: [int]) -> Union[str, None]:
         """
         Returns either warning message or plots model results and observed values against X set as a scatter and line
         plot respectively.
@@ -285,8 +300,8 @@ class Moons:
         reg_model = model[0]
         data_for_predict = model[1]
         y_col = model[2]
-        plt.scatter(data_for_predict, y_col, label = f"{model[3]} test data", marker = "x")
-        plt.plot(data_for_predict, pred, label = f"Prediction", color = "orange")
+        plt.scatter(data_for_predict, y_col, label=f"{model[3]} test data", marker="x")
+        plt.plot(data_for_predict, pred, label=f"Prediction", color="orange")
         # Add axis labels and title
         plt.title(f"{model[3]} & prediction x {model[3]}")
         plt.xlabel(model[3])
@@ -294,7 +309,7 @@ class Moons:
         plt.legend()
 
     @staticmethod
-    def plot_residuals(model:tuple, pred: pd.array) -> None:
+    def plot_residuals(model: tuple, pred: [int]) -> None:
         """
         Plots residuals between observed target values and predicted target values against X test data.
         This is calculated through subtracting the observed target values by the predicted.
@@ -315,7 +330,7 @@ class Moons:
         plt.ylabel("Residuals")
 
     @staticmethod
-    def output_model_worth(model:tuple, pred: pd.array) -> (int):
+    def output_model_worth(model: tuple, pred: [int]) -> tuple[float | Any, float | Any]:
         """
         Returns R^2 and Root Mean Squared Error. Methods imported from sklearn library
         :param model: Tuple generated from self.get_model(). In order,
@@ -328,7 +343,7 @@ class Moons:
         :return: Tuple with R^2 value and root mean squared speed.
         """
         r2 = r2_score(model[2], pred)
-        rmse = mean_squared_error(model[2], pred, squared = False)
+        rmse = mean_squared_error(model[2], pred, squared=False)
         print(f"r2_score: {r2}")
         print(f"root mean squared error: {rmse}")
         return r2, rmse
